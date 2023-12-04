180ead80e045ebf40c1c60e9427b396519ec7721
Samuel <samuel.okafor@decagon.dev>
2023-12-05 00:58:11 +0100

    customer service

--- src/main/java/com/decagon/fitnessoapp/exception/CustomServiceExceptions.java
+++ src/main/java/com/decagon/fitnessoapp/exception/CustomServiceExceptions.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CustomServiceExceptions extends RuntimeException{
-
     public CustomServiceExceptions (String message){
         super(message);
     }
